b832876bc28f3512c44c37425614cb450b70aa17
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-08-28 11:52:08 -0600

    se agredo CRUD

--- src/org/pltres/controller/BodegaDashboardController.java
+++ src/org/pltres/controller/BodegaDashboardController.java
@@ -6,14 +6,25 @@ package org.pltres.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author informatica
- */
 public class BodegaDashboardController implements Initializable {
+
+    @FXML
+    private Label lblUsuario;
+    @FXML
+    private Button btnCerrarSesion;
+    @FXML
+    private Button btnNavLibros;
+    @FXML
+    private Button btnNavIngreso;
+    @FXML
+    private Button btnNavSalida;
+    @FXML
+    private Button btnNavMovimientos;
 
     /**
      * Initializes the controller class.
--- src/org/pltres/dao/UsuarioDAO.java
+++ src/org/pltres/dao/UsuarioDAO.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.pltres.dao;
+import org.pltres.model.Usuario;
+import org.pltres.util.Conexion;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.CallableStatement;
 
-/**
- *
- * @author informatica
- */
-public class UsuarioDAO {
+
+
+public class UsuarioDAO {    
+    
+    public Usuario iniciarSesion(String usernarme, String passwordHash){                
+        Usuario usuario = null;
+        String sql = "{call sp_iniciar_sesion(?,?)}";
+        
+        
+        try (Connection conexion = Conexion.getInstancia().conectar();
+                CallableStatement consulta = conexion.prepareCall(sql)){
+            
+            consulta.setString(1, usernarme);
+            consulta.setString(2, passwordHash);
+            
+            try(ResultSet tablaResultado = consulta.executeQuery()){
+                
+                if (tablaResultado.next()) {
+              
+                    usuario = new Usuario();
+                    usuario.setId(tablaResultado.getInt(1));
+                    usuario.setUsername(tablaResultado.getString(2));
+                    usuario.setRol(tablaResultado.getString(3));
+                }
+            }            
+        } catch (SQLException e) {
+            System.err.println("Error en Iniciar Sesion: " + e.getMessage());                   
+        }
+        
+        return usuario;
+    }
+        public boolean registrarUsuario(String username, String passwordHash){
+        String sql = "{call sp_registrar_usuario(?,?,'Empleado')}";
+        try (Connection conexion = Conexion.getInstancia().conectar();
+                CallableStatement consulta = conexion.prepareCall(sql)){
+            consulta.setString(1, username);
+            consulta.setString(2, passwordHash);
+            int filasAfectadas = consulta.executeUpdate();
+            return filasAfectadas > 0;
+        } catch (SQLException e) {
+            System.err.println("Error en Registrar Usuario: " + e.getMessage());
+            return false;
+        }
+        
+    }
     
 }
